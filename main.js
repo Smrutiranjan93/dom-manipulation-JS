e62e501ceaceed404ea0b8860b6445879c79f862
Smrutiranjan93 <103051369+Smrutiranjan93@users.noreply.github.com>
2023-02-16 08:48:44 +0530

    query

--- main.js
+++ main.js
@@ -25,18 +25,50 @@
 //     }
 // }))
 // get element by id
-console.log(document.getElementById('header-title'));
-var header=document.getElementById('header-title');
-var header1=document.getElementById('main-header');
-header.textContent="hello";
-header.innerText="goodbye";
-header.innerHTML='<h3>Good Bye </h3>';
-header.style.borderBottom='solid 3px red';
-header1.style.borderBottom='solid 3px red';
+// console.log(document.getElementById('header-title'));
+// var header=document.getElementById('header-title');
+// var header1=document.getElementById('main-header');
+// header.textContent="hello";
+// header.innerText="goodbye";
+// header.innerHTML='<h3>Good Bye </h3>';
+// header.style.borderBottom='solid 3px red';
+// header1.style.borderBottom='solid 3px red';
+
 // get element by classname
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent="hello 3";
-items[1].style.fontweight='bolder';
-items[2].style.backgroundColor="green"
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent="hello 3";
+// items[1].style.fontweight='bolder';
+// items[2].style.backgroundColor="green"
+// get element by tag name
+
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent="hello 3";
+// li[1].style.fontweight='bolder';
+// for(var i=0;i<li.length;i++){
+
+//     li[i].style.backgroundColor="gray"
+// }
+// get element by query selector
+// var header=document.querySelector("#main-header")
+// header.style.borderBottom="solid 4px red";
+// var input=document.querySelector('input');
+// input.value='hello smruti';
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value="send";
+// var item=document.querySelector('.list-group-item');
+// item.style.color='yellow'
+// var lastItem=document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue'
+// var secondItem=document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.color='red';
+// get element by queryselectorall
+var titles=document.querySelectorAll('.title')
+console.log(titles)
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='red'
+}
