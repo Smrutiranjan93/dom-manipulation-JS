bc3422c235a61817969968d17d424ecbccc2c288
Smrutiranjan93 <103051369+Smrutiranjan93@users.noreply.github.com>
2023-03-21 08:36:49 +0530

    edit functionality addedd

--- main.js
+++ main.js
@@ -4,26 +4,27 @@
 // firstchild.style.color='green';
 // ul.children[1].style.color='yellow';
 
-// const myForm=document.querySelector('#my-form');
-// const userName=document.querySelector('#name');
-// const userMsg=document.querySelector('.msg');
-// const userEmail=document.querySelector('#email');
-// const userList=document.querySelector('#users');
-// myForm.addEventListener('submit',((e)=>{
-//     e.preventDefault();
-//     if(userName.value ==='' || userEmail.value ===''){
-//         userMsg.classList.add('error');
-//         userMsg.innerHTML='please enter all fields';
-//         setTimeout(()=>
-//             userMsg.remove(),3000)
-//     } else{
-//         const li=document.createElement('li');
-//         li.appendChild(document.createTextNode(`${userName.value}:${userEmail.value}`))
-//         userList.appendChild(li);
-//         userEmail.value='';
-//         userName.value='';
-//     }
-// }))
+const myForm=document.querySelector('#my-form');
+const userName=document.querySelector('#name');
+const userMsg=document.querySelector('.msg');
+const userEmail=document.querySelector('#email');
+const userList=document.querySelector('#users');
+myForm.addEventListener('submit',((e)=>{
+    e.preventDefault();
+    if(userName.value ==='' || userEmail.value ===''){
+        userMsg.classList.add('error');
+        userMsg.innerHTML='please enter all fields';
+        setTimeout(()=>
+            userMsg.remove(),3000)
+    } else{
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${userName.value}:${userEmail.value}`))
+        localStorage.setItem(`${userName.value}`,`${userEmail.value}`);
+        userList.appendChild(li);
+        userEmail.value='';
+        userName.value='';
+    }
+}))
 // get element by id
 // console.log(document.getElementById('header-title'));
 // var header=document.getElementById('header-title');
---
+++ object_localstorage.js
@@ -0,0 +1,11 @@
+// let myObj={
+//     name:'smruti',
+//     age:29
+// }
+// console.log(myObj);
+// const serializedData=JSON.stringify(myObj)
+// storing object
+// localStorage.setItem('myObj',serializedData)
+// const deserializedData=JSON.parse(localStorage.getItem('myObj'))
+// console.log(serializedData);
+// console.log(deserializedData);
